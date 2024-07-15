bac851f5dae6646aa944bd0a5e383268dce37fcb
taylor <taylor.laekeman@gmail.com>
2024-07-15 14:47:48 -0400

    remove journal directory

---
+++ apps/ui/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './page.module.css';
+
+export default function Index(): React.ReactElement {
+  return (
+    <main className={styles.page}>
+      <h1>Journal</h1>
+    </main>
+  );
+}
