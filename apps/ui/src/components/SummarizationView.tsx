2bfd4f942d27770c0aa11b1b947b81eae57ef832
taylor <taylor.laekeman@gmail.com>
2024-07-17 16:08:17 -0400

    add SummarizationView component

---
+++ apps/ui/src/components/SummarizationView.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { InputPanel } from './InputPanel';
+import { Panel } from './Panel';
+import styles from './SummarizationView.module.css';
+import { SummaryPanel } from './SummaryPanel';
+import { Border } from '../utils/border';
+import { noop } from '../utils/noop';
+import { Padding } from '../utils/padding';
+
+export function SummarizationView({
+  entries = {},
+  expandedEntry = '',
+  onChangeSummary = noop,
+  onExpandEntry = noop,
+  summary = '',
+  summaryTitle,
+}: {
+  entries?: Record<string, string>;
+  expandedEntry?: string;
+  onChangeSummary?: (input: string) => void;
+  onExpandEntry?: (input: string) => void;
+  summary?: string;
+  summaryTitle?: string;
+}): React.ReactElement {
+  return (
+    <Panel padding={Padding.None}>
+      <SummaryPanel
+        border={Border.OnlyBottom}
+        entries={entries}
+        expandedEntry={expandedEntry}
+        onExpandEntry={onExpandEntry}
+      />
+      <InputPanel
+        border={Border.None}
+        onChange={onChangeSummary}
+        title={summaryTitle}
+        value={summary}
+      />
+    </Panel>
+  );
+}
